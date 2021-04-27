a1a87b34c8ae33fc80afbfcf9bb97150ea45c959
Jonathan Lambert <jonathanlambert247@gmail.com>
2021-04-27 09:36:24 -0700

    fixed group styling issue

---
+++ src/components/Group.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import PhotoThumbnail from "../components/PhotoThumbnail";
+import { Entypo } from "@expo/vector-icons";
+
+const Group = ({ result }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        marginBottom: 10,
+        paddingLeft: 10,
+        paddingTop: 10,
+      }}
+    >
+      {result.banner ? (
+        <PhotoThumbnail
+          uri={result.sender.profile_picture}
+          width={50}
+          height={50}
+        />
+      ) : (
+        <PhotoThumbnail width={60} height={60} />
+      )}
+      <View style={{ flex: 1, alignSelf: "center" }}>
+        <Text
+          style={{
+            marginLeft: 11,
+            fontSize: 17,
+            marginBottom: 2,
+            fontWeight: "500",
+          }}
+        >
+          {result.name}
+        </Text>
+        <Text style={{ marginLeft: 11, color: "#9f9f9f" }}>
+          {result.members}{" "}
+          {result.members == 1 ? <Text>member</Text> : <Text>members</Text>} · 0
+          posts
+        </Text>
+      </View>
+      <Entypo
+        style={{ alignSelf: "center", marginRight: 15 }}
+        name="chevron-right"
+        size={22}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Group;
--- src/screens/CreatedGroupsScreen.js
+++ src/screens/CreatedGroupsScreen.js
@@ -1,8 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, FlatList } from "react-native";
+import thimbleApi from "../api/thimble";
+import Group from "../components/Group";
 
-const CreatedGroupsScreen = () => {
-  return <View></View>;
+const CreatedGroupsScreen = ({ navigation }) => {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const fetchGroups = navigation.addListener("focus", async () => {
+      try {
+        const response = await thimbleApi.get("g/created");
+        setResults(response.data);
+      } catch (error) {}
+    });
+
+    return fetchGroups;
+  }, [navigation]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={results}
+        keyExtractor={(result) => result.uuid}
+        renderItem={({ item }) => {
+          return <Group result={item} />;
+        }}
+      />
+    </View>
+  );
 };
 
 export default CreatedGroupsScreen;
