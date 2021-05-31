fd3dfb945258be1fe3585e1c1189ac785ab4bd04
Jonathan Lambert <jonathanlambert247@gmail.com>
2021-05-31 12:53:14 -0700

    started profile design

--- src/navigation/ProfileFlow.js
+++ src/navigation/ProfileFlow.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native";
 import ProfileScreen from "../screens/ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const ProfileFlow = createStackNavigator();
 
-const ProfileFlowStack = () => (
+const ProfileFlowStack = ({ navigation }) => (
   <ProfileFlow.Navigator>
-    <ProfileFlow.Screen name="Profile" component={ProfileScreen} />
+    <ProfileFlow.Screen
+      options={{
+        headerRight: () => (
+          <TouchableOpacity>
+            <Ionicons
+              name="settings-outline"
+              size={26}
+              color="black"
+              style={{ marginRight: 15 }}
+            />
+          </TouchableOpacity>
+        ),
+      }}
+      name="Profile"
+      component={ProfileScreen}
+    />
   </ProfileFlow.Navigator>
 );
 
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,15 +1,56 @@
-import React, { useContext } from "react";
-import { View, StyleSheet } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Avatar } from "react-native-elements";
 import { Button } from "react-native-elements";
-import { Context as AuthContext } from "../context/AuthContext";
 
 const ProfileScreen = () => {
-  const { logout } = useContext(AuthContext);
-
   return (
-    <View>
-      <Button title="log out" onPress={() => logout()} />
-    </View>
+    <ScrollView style={{ backgroundColor: "#fff" }}>
+      <View style={{ alignItems: "center", marginTop: 25 }}>
+        <Avatar
+          containerStyle={{
+            borderWidth: 0.5,
+            borderColor: "#d3d3d3",
+          }}
+          rounded
+          icon={{ name: "user", type: "feather", color: "#333" }}
+          size={80}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          marginTop: 25,
+          alignItems: "center",
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold" }}>0</Text>
+          <Text style={{ fontSize: 16 }}>posts</Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold" }}>0</Text>
+          <Text style={{ fontSize: 16 }}>groups</Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold" }}>0</Text>
+          <Text style={{ fontSize: 16 }}>friends</Text>
+        </View>
+      </View>
+      <View style={{ marginTop: 25, paddingHorizontal: 30 }}>
+        <Button
+          buttonStyle={{
+            borderColor: "#A6A3FF",
+            borderWidth: 1,
+            paddingVertical: 8,
+          }}
+          titleStyle={{ color: "#A6A3FF", fontSize: 16, fontWeight: "500" }}
+          title="Edit Profile"
+          type="outline"
+        />
+      </View>
+    </ScrollView>
   );
 };
 
