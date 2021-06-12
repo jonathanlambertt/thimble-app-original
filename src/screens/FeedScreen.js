2013bd205d56d2e483f8feaaee8672adfaa7324f
Jonathan Lambert <jonathanlambert247@gmail.com>
2021-06-12 00:18:26 -0700

    feed is now functional

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,8 +1,41 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import thimbleApi from "../api/thimble";
+import Post from "../components/Post";
 
-const FeedScreen = () => {
-  return <View></View>;
+const FeedScreen = ({ navigation }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchFeed();
+  }, []);
+
+  useEffect(() => {
+    const callFetchFeed = navigation.addListener("focus", async () => {
+      fetchFeed();
+    });
+
+    return callFetchFeed;
+  }, [navigation]);
+
+  const fetchFeed = async () => {
+    try {
+      const response = await thimbleApi.get("u/feed");
+      setPosts(response.data);
+    } catch (error) {}
+  };
+
+  return (
+    <View>
+      <FlatList
+        data={posts}
+        keyExtractor={(post) => post.uuid}
+        renderItem={({ item }) => {
+          return <Post post={item} />;
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
