fd12f4c57841df8ccf76999176ae1ce77355ec06
Jonathan Lambert <jonathanlambert247@gmail.com>
2021-06-14 07:19:51 -0700

    added under construction on new post screen

--- src/navigation/NewPostFlow.js
+++ src/navigation/NewPostFlow.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import NewPostScreen from "../screens/NewPostScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Feather } from "@expo/vector-icons";
 
 const NewPostFlow = createStackNavigator();
 
-const NewPostFlowStack = () => (
+const NewPostFlowStack = ({ navigation: { goBack } }) => (
   <NewPostFlow.Navigator>
     <NewPostFlow.Screen
       name="New Post"
       component={NewPostScreen}
-      options={{ headerBackTitle: "Back" }}
+      options={{
+        headerBackTitleVisible: false,
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => goBack()}>
+            <Feather
+              name="x"
+              size={26}
+              color="black"
+              style={{ marginLeft: 17 }}
+            />
+          </TouchableOpacity>
+        ),
+      }}
     />
   </NewPostFlow.Navigator>
 );
--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const NewPostScreen = () => {
-  return <View></View>;
+  return (
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 10 }}>
+        Under Construction
+      </Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
